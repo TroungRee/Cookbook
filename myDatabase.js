60c94690f22ca3fff722d0a82b80d539c791d672
TruongRee <leee4216@gmail.com>
2021-04-23 10:34:49 -0700

    WE ARE FINALLY DONE

--- myDatabase.js
+++ myDatabase.js
@@ -6,7 +6,7 @@ let myDatabase = function() {
 }
 
 let recipeIndex = 0;
-let index = 1;
+let index = 0;
 myDatabase.prototype.displayRecipes = function() {
 	for (let i=0;i<this.recipes.length;i++) {
 		console.log(this.recipes[i]);
@@ -19,6 +19,12 @@ myDatabase.prototype.postRecipe = function(recipe) {
       return false;
     }
   }
+
+	if(recipe.index >= 4){
+			this.recipes[recipeIndex++] = new Recipe(recipe.dish,recipe.ingredients,recipe.directions,recipe.category,recipe.image,recipe.index);
+			return true;
+	}
+
 //	this.recipes[recipeIndex++] = recipe;
 	this.recipes[recipeIndex++] = new Recipe(recipe.dish,recipe.ingredients,recipe.directions,recipe.category,recipe.image,index);
 	index++;
@@ -27,9 +33,7 @@ myDatabase.prototype.postRecipe = function(recipe) {
 
 myDatabase.prototype.getRecipeByName = function(dish) {
   for (let i=0;i<this.recipes.length;i++) {
-    if (this.recipes[i] && dish == this.recipes[i].dish)
-		{
-//			return(this.recipes[i]);
+    if (this.recipes[i] && dish == this.recipes[i].dish) {
       return(new Recipe(this.recipes[i].dish,this.recipes[i].ingredients,this.recipes[i].directions,this.recipes[i].category,this.recipes[i].image,this.recipes[i].index));
 		}
   }
@@ -38,20 +42,11 @@ myDatabase.prototype.getRecipeByName = function(dish) {
 
 myDatabase.prototype.getRecipeByIndex = function(index) {
   for (let i=0;i<this.recipes.length;i++) {
-    if (this.recipes[i] && index == this.recipes[i].index)
-		{
-//			return(this.students[i]);
+    if (this.recipes[i] && index == this.recipes[i].index) {
       return(new Recipe(this.recipes[i].dish,this.recipes[i].ingredients,this.recipes[i].directions,this.recipes[i].category,this.recipes[i].image,this.recipes[i].index));
 		}
   }
 	return null;
 }
-/*
-myDatabase.prototype.getRecentRecipe = function() {
-  if (this.recipes.length != 0) {
-    return(new Recipe(this.recipes[this.recipes.length-1].dish,this.recipes[this.recipes.length-1].ingredients,this.recipes[this.recipes.length-1].directions,this.recipes[this.recipes.length-1].category,this.recipes[this.recipes.length-1].image));
-	}
-	return null;
-}
-*/
+
 module.exports = myDatabase;
