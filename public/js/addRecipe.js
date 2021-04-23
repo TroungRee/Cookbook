2b53a3ee261aee1e27301f7b7568132e3a23f596
TruongRee <leee4216@gmail.com>
2021-04-23 10:42:02 -0700

    WE ARE ACTUALLY FINALLY DONE NOW

--- public/js/addRecipe.js
+++ public/js/addRecipe.js
@@ -7,14 +7,6 @@ function createClicked(){
       data: {dish:$("#dish").val(),ingredients:$("#ingredients").val(),
              directions:$("#directions").val(),category:$("#category").val(),
              image:$("#imageStuff").val().substring(12), index:0},
-      success: function(data){
-        if (!data)
-          alert("bad create");
-        else if (data.retVal)
-          alert("good create");
-        else
-          alert("bad create");
-        } ,
       dataType: "json"
     });
   return false;
